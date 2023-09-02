5b4ac72e3d97ce12c6303ccc6fcea2c2c2b4ec55
mohammed <engmohameedmabhooh@gmail.com>
2023-09-02 11:05:26 +0300

    start1

---
+++ android/app/src/main/kotlin/com/example/ahm_dachbord/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ahm_dachbord
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/splash.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class splash extends StatelessWidget {
+  const splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
